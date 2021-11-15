b67475e9774937ad509a9b5df6205b93c217fe0c
painteyes <andreantoniocchipinti@gmail.com>
2021-11-15 16:15:55 +0100

    commit changes

--- main.js
+++ main.js
@@ -5,14 +5,26 @@
 // dovete solo aggiungere la parte js per far si che il menu su mobile diventi visibile al click sull'hamburger menu :)
 
 
-const hamburgerMenuEvent = document.querySelector ('.fas');
-console.log(hamburgerMenuEvent);
-const hamburgerMenu = document.querySelector ('.hamburger-menu');
+const hamburgerMenu = document.querySelector ('.fas');
 console.log(hamburgerMenu);
+const hamburgerMenuOpen = document.querySelector ('.hamburger-menu');
+console.log(hamburgerMenuOpen);
 
 
-hamburgerMenuEvent.addEventListener ( 'click', 
+hamburgerMenu.addEventListener ( 'click', 
     function() {
-        hamburgerMenu.style.dispaly ='block';
+        hamburgerMenuOpen.style.dispaly ='block';
     }
 );
+
+
+const hamburgerMenuClose = document.querySelector ('.close');
+console.log(hamburgerMenuClose);
+
+
+hamburgerMenuClose.addEventListener ( 'click', 
+    function() {
+        hamburgerMenuOpen.style.dispaly ='none';
+    }
+);
+
